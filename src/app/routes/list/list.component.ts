a9d25eae78291745c00e6dc8cf02c95bd53628a6
davembush <davembush@dmbcllc.com>
2017-10-13 11:34:23 -0400

    step-9 - list route

--- src/app/routes/list/list.component.ts
+++ src/app/routes/list/list.component.ts
@@ -1,3 +1,4 @@
+import { Contact } from './contact';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  contacts: ReadonlyArray<Contact> = [
+    {
+      id: 1,
+      firstName: 'Dave',
+      lastName: 'Bush',
+      dateOfBirth: new Date(2000, 0, 15)
+    },
+    {
+      id: 2,
+      firstName: 'John',
+      lastName: 'Dough',
+      dateOfBirth: new Date(1990, 5, 15)
+    }
+  ];
 
   constructor() {
   }
