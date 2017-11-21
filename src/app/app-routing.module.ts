9f56475224964f578877f4605bd4ad0c1d0d730e
davembush <davembush@dmbcllc.com>
2017-11-21 09:05:45 -0500

    step-5 - Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  redirectTo: 'list',
+  pathMatch: 'full'
+}, {
+  path: 'list',
+  loadChildren:
+    './routes/list/list.module#ListModule'
+}, {
+  path: 'edit',
+  loadChildren:
+    './routes/edit/edit.module#EditModule'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
