bbf0f3135460ba86b68e0e1fd7bef641d8fcccab
davembush <davembush@dmbcllc.com>
2017-11-22 09:35:48 -0500

    step-8 - Styling the App

--- src/app/routes/list/list.component.ts
+++ src/app/routes/list/list.component.ts
@@ -1,7 +1,6 @@
 import { AppState } from '../../app-state';
 import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
-import * as Errors from '../../shared/errors/errors.actions';
 
 @Component({
   selector: 'app-list',
@@ -13,11 +12,6 @@ export class ListComponent implements OnInit {
   constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
-  }
-
-  start(): void {
-    this.store.dispatch(
-      new Errors.Add('Here is an error'));
   }
 
 }
