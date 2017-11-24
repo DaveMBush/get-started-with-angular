1174ef9b1dbe0b542e6aa3d7ab637030e12ecc9a
davembush <davembush@dmbcllc.com>
2017-11-24 12:32:22 -0500

    missing file

--- src/app/routes/edit/edit.service.spec.ts
+++ src/app/routes/edit/edit.service.spec.ts
@@ -1,11 +1,37 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import { EffectsModule } from '@ngrx/effects';
+import { SharedModule } from '../../shared/shared.module';
+import { APP_BASE_HREF } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { EditReducer } from './edit.reducer';
+import { EditState } from './edit-state';
+import { ActionReducerMap, StoreModule } from '@ngrx/store';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { inject, TestBed } from '@angular/core/testing';
 import { EditService } from './edit.service';
 
+const reducers:
+  ActionReducerMap<EditState> = {
+    form: EditReducer
+  }
 describe('EditService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [EditService]
+      imports: [
+        StoreModule.forRoot({}),
+        StoreModule.forFeature('edit', reducers),
+        EffectsModule.forRoot([]),
+        RouterModule.forRoot([]),
+        ReactiveFormsModule,
+        FormsModule,
+        SharedModule
+      ],
+      providers: [
+        {
+          provide: APP_BASE_HREF,
+          useValue: '/'
+        },
+        EditService
+      ]
     });
   });
 
