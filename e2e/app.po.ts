dd0ec110da1ae0a6d46adf19b9fd8e33d88c2bbe
davembush <davembush@dmbcllc.com>
2017-11-24 12:39:56 -0500

    step-25 E2E Testing

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getParagraphText(): promise.Promise<string> {
-    return element(by.css('app-root h1')).getText();
+  getHeaderText(): promise.Promise<string> {
+    return element(by.css('app-root .navbar-brand')).getText();
   }
 }
